ee3a91628303766b0d4d6c099de3486d530e8892
Brian Kephart <briantkephart@gmail.com>
2024-07-03 16:10:14 -0500

    Specify platform in Docker build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
-docker build --no-cache --pull -t heroku-imagemagick container
+docker build --platform linux/amd64 -t heroku-imagemagick container
 mkdir -p build
-docker run --rm -t -v $PWD/build:/data heroku-imagemagick sh -c 'cp -f /usr/src/imagemagick/build/*.tar.bz2 /data/'
+docker run --platform linux/amd64 --rm -t -v $PWD/build:/data heroku-imagemagick sh -c 'cp -f /usr/src/imagemagick/build/*.tar.bz2 /data/'
