e184b2356d94f2da786dc8f19eb78cebd9c08d9d
Brian Kephart <briantkephart@gmail.com>
2024-07-04 00:12:22 -0500

    Only recompile imagemagick since supporting libraries are already present

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 docker build --platform linux/amd64 -t heroku-imagemagick container
+docker run --platform linux/amd64 -it heroku-imagemagick
 mkdir -p build
 docker run --platform linux/amd64 --rm -t -v $PWD/build:/data heroku-imagemagick sh -c 'cp -f /usr/src/imagemagick/build/*.tar.bz2 /data/'
